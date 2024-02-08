5a8d438d01f8d4e9838fa0e5bfb86fbccaabc4ed
Pedro Vieira <pedro.silva@pagtel.com.br>
2024-02-08 15:53:44 -0300

    feat: ensure AxiosHttpClient calls axios with correct values (verb, url and body)

--- src/infra/http/axiosHttpClient/axios-http-client.ts
+++ src/infra/http/axiosHttpClient/axios-http-client.ts
@@ -1,13 +1,18 @@
-import { type HttpPostParams } from '@/data/protocols/http'
+import { type HttpPostClient, type HttpPostParams, type HttpResponse } from '@/data/protocols/http'
 import axios from 'axios'
 
-export class AxiosHttpClient {
+export class AxiosHttpClient implements HttpPostClient<any, any> {
   private readonly axios: typeof axios
   constructor () {
     this.axios = axios
   }
 
-  async post (params: HttpPostParams<any>): Promise<void> {
-    await this.axios.post(params.url, params.body)
+  async post (params: HttpPostParams<any>): Promise<HttpResponse<any>> {
+    const httpResponse = await this.axios.post(params.url, params.body)
+
+    return {
+      statusCode: httpResponse.status,
+      body: httpResponse.data
+    }
   }
 }
