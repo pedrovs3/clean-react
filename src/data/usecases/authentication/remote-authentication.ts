06aace6ad256b2e1b1a2381ade77a4324dda02b0
Pedro Vieira <pedro.silva@pagtel.com.br>
2024-02-08 14:38:45 -0300

    feat: ensure RemoteAuthentication  return an Account on success

--- src/data/usecases/authentication/remote-authentication.ts
+++ src/data/usecases/authentication/remote-authentication.ts
@@ -1,25 +1,25 @@
-import { type HttpPostClient, type HttpResponse, HttpStatusCode } from '@/data/protocols/http'
-import { type AuthenticationParams } from '@/domain/usecases/authentication'
+import { type HttpPostClient, HttpStatusCode } from '@/data/protocols/http'
+import { type Authentication, type AuthenticationParams } from '@/domain/usecases/authentication'
 import { InvalidCredentialsError } from '@/domain/errors/invalid-credentials.error'
 import { NetworkError, UnexpectedError } from '@/domain/errors'
 import { type AccountModel } from '@/domain/models/account.model'
 
-export class RemoteAuthentication {
+export class RemoteAuthentication implements Authentication {
   constructor (
     private readonly url: string,
     private readonly httpPostClient: HttpPostClient<AuthenticationParams, AccountModel>) {
   }
 
-  async auth (params: AuthenticationParams): Promise<HttpResponse<AccountModel>> {
+  async auth (params: AuthenticationParams): Promise<AccountModel | undefined> {
     const htttpResponse = await this.httpPostClient.post({ url: this.url, body: params })
 
     switch (htttpResponse.statusCode) {
       case HttpStatusCode.unauthorized: throw new InvalidCredentialsError()
       case HttpStatusCode.badRequest: throw new UnexpectedError()
       case HttpStatusCode.serverError: throw new NetworkError()
-      case HttpStatusCode.ok: break
-      default: return htttpResponse
+      case HttpStatusCode.ok: return htttpResponse.body
+      case HttpStatusCode.noContent: return undefined
+      default: throw new UnexpectedError()
     }
-    return htttpResponse
   }
 }
