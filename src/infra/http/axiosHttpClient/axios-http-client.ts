ee78d04f698996d8c68fb786910744c80ab082bd
Pedro Vieira <pedro.silva@pagtel.com.br>
2024-02-16 15:30:29 -0300

    feat: add http methods to axios client

--- src/infra/http/axiosHttpClient/axios-http-client.ts
+++ src/infra/http/axiosHttpClient/axios-http-client.ts
@@ -10,6 +10,8 @@ export class AxiosHttpClient implements HttpPostClient<any, any> {
   private readonly axios: typeof axios;
   constructor() {
     this.axios = axios;
+    this.requestInterceptors();
+    this.responseInterceptors();
   }
 
   async post(params: HttpPostParams<any>): Promise<HttpResponse<any>> {
@@ -20,4 +22,54 @@ export class AxiosHttpClient implements HttpPostClient<any, any> {
       statusCode: httpResponse.status,
     };
   }
+
+  async get(params: HttpPostParams<any>): Promise<HttpResponse<any>> {
+    const httpResponse = await this.axios.get(params.url);
+
+    return {
+      body: httpResponse.data,
+      statusCode: httpResponse.status,
+    };
+  }
+
+  async put(params: HttpPostParams<any>): Promise<HttpResponse<any>> {
+    const httpResponse = await this.axios.put(params.url, params.body);
+
+    return {
+      body: httpResponse.data,
+      statusCode: httpResponse.status,
+    };
+  }
+
+  async delete(params: HttpPostParams<any>): Promise<HttpResponse<any>> {
+    const httpResponse = await this.axios.delete(params.url);
+
+    return {
+      body: httpResponse.data,
+      statusCode: httpResponse.status,
+    };
+  }
+
+  async patch(params: HttpPostParams<any>): Promise<HttpResponse<any>> {
+    const httpResponse = await this.axios.patch(params.url, params.body);
+
+    return {
+      body: httpResponse.data,
+      statusCode: httpResponse.status,
+    };
+  }
+
+  requestInterceptors() {
+    axios.interceptors.request.use((config) => {
+      return config;
+    });
+  }
+
+  responseInterceptors() {
+    axios.interceptors.response.use((response) => {
+      return response;
+    });
+  }
 }
+
+export const axiosHttpClient = new AxiosHttpClient();
